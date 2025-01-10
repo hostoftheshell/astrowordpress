2be2e376c36722e349605b4deee9750846e2849d
bap <baptiste.chenin.dev@gmail.com>
2025-01-10 14:03:26 +0100

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  image: {
+    domains: ["astro.build", "localhost:10068"],
+    remotePatterns: [{ protocol: "http" }],
+  }
+});
